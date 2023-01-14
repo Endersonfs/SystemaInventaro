30a65338041852160d6de2c792033af8c868f34d
Enderson Florian Solano <efloriansolano@gmail.com>
2023-01-14 02:30:02 -0400

    primer commit

---
+++ SystemaInventario/SystemaInventario/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SystemaInventario.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
