99861fc788ff29c2ebb5aa1d30740dfa39a223f6
Enderson Florian Solano <efloriansolano@gmail.com>
2023-01-14 20:01:26 -0400

    Separando los datos del sistama

--- SystemaInventario/SystemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
+++ SystemaInventario/SystemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace SystemaInventario.Data
+namespace SystemaInventario.AccesoDatos.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
